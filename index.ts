074b53b198e25f40de74386681590ddcf1a0191a
maff1989 <matthew@urgero.net>
2025-12-23 00:50:50 -0700

    Migrate `index.ts` package entry point
    Forgot to add this back in before. Now this package can be dynamically imported where necessary.

---
+++ index.ts
@@ -0,0 +1,8 @@
+/**
+ * Copyright 2025 The Lotusia Stewardship
+ * Github: https://github.com/LotusiaStewardship
+ * License: MIT
+ */
+export * from './lib/musig2'
+export * from './lib/discovery'
+export * from './lib'
